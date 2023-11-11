e5858c1c108cb33e29f61b87660fa0f548549b4f
Andrey Marques <andreymarques822@gmail.com>
2023-11-11 17:47:37 -0300

    Backend: Criação das rotas

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const checklistsRouter = require("./src/routes/Checklist")
+
+const app = express();
+
+app.use(express.json())
+
+app.use("/checklists", checklistsRouter);
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado");
+})

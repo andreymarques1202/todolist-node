2de65932a4e75525af56350c88902a2472fc7439
Andrey Marques <andreymarques822@gmail.com>
2023-11-13 17:15:45 -0300

    Backend: Configurando models e rotas

--- src/routes/Checklist.js
+++ src/routes/Checklist.js
@@ -1,19 +1,32 @@
 const express = require("express");
 
 const router = express.Router();
+const Checklist = require("../models/Checklist");
 
-router.get("/", (req, res) => {
-    console.log("ola");
-    res.send();
+router.get("/", async (req, res) => {
+    try {
+        let checklist = await Checklist.find({});
+        res.status(200).send(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 })
 
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body );
+router.post("/", async (req, res) => {
+    let {name} = req.body;
+
+    try {
+         let checklist = await Checklist.create({name});
+         res.status(200).send(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+   
+    
 })
 
 router.get("/:id", (req, res) => {
-    console.log(req.params.id);
+    console.log(req.body["name"]);
     res.send(`ID: ${req.params.id}`);
 })
 

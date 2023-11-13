ad92311eb08ea56253c1849d3645eeb2c4139185
Andrey Marques <andreymarques822@gmail.com>
2023-11-13 20:41:47 -0300

    Backend: configurando as rotas

--- src/routes/Checklist.js
+++ src/routes/Checklist.js
@@ -34,4 +34,23 @@ router.get("/:id", async (req, res) => {
     }
 })
 
+router.put("/:id", async (req, res) => {
+    let {name} = req.body;
+    try {
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    try {
+        let checklist = await Checklist.findByIdAndRemove(req.params.id)
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
 module.exports = router;

3cff8b6238cfefbbd6ad7f520c5e722799f8a327
Andrey Marques <andreymarques822@gmail.com>
2023-11-13 21:25:43 -0300

    Backend: Renderizando a pagina index com ejs

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const path = require("path")
 const checklistsRouter = require("./src/routes/Checklist")
+const rootRouter = require("./src/routes/index");
 require("./config/Database");
 
 const app = express();
-
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "src/views"))
+app.set("view engine", "ejs")
+
 app.use("/checklists", checklistsRouter);
+app.use("/", rootRouter)
 
 app.listen(3000, () => {
     console.log("Servidor iniciado");

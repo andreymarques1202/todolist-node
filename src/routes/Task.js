b39517ce2f94992099a5d95ab34c0c55cebc9ae7
Andrey Marques <andreymarques822@gmail.com>
2023-11-15 20:14:37 -0300

    Backend: Fazendo request JSON

--- src/routes/Task.js
+++ src/routes/Task.js
@@ -26,6 +26,18 @@ simpleRouter.delete("/:id", async (req, res) => {
     }
 })
 
+simpleRouter.put("/:id", async (req, res) => {
+    let task = await Task.findById(req.params.id);
+    try {
+        task.set(req.body.task);
+        await task.save();
+        res.status(200).json({task});
+    } catch (error) {
+        let errors = error.errors;
+        res.status(422).json({task: {...errors}})
+    }
+})
+
 checklistDependentRoute.post("/:id/tasks", async (req, res) => {
     let {name} = req.body.task;
     let task = new Task({name, checklist: req.params.id})
